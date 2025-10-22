bb2480178b6811c14412b49f8dba64d0e17bad4e
ordinaryYT <baileybailey698@gmail.com>
2025-10-22 14:21:16 +0100

    Delete migration.sql

--- migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS movies (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  imdb_id TEXT UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
-  year TEXT,
-  image TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
-);
